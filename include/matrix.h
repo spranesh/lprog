be91805ffac321e517f3bd2691cc4a4e3d32e635
Pranesh Srinivasan <spranesh@gmail.com>
2008-06-15 10:40:07 +0530

    Wrote the Matrix Function declaration. Needs to be filled in

--- include/matrix.h
+++ include/matrix.h
@@ -15,4 +15,62 @@
  */
 
 
-void ExampleFunction();
+# ifndef MATRIX_H
+# define MATRIX_H
+
+# include<vector>
+using namespace std;
+
+template <typename T>
+class Matrix
+{
+	public:
+		// We define as a double dimensional array. This should
+		// speed up things quite a lot
+		T** matrix;
+
+
+		void Matrix(int n, int m);
+
+		// We first overload operators as shown below.
+		// If you are new to this, I suggest you have a look at 
+		// http://www.cs.caltech.edu/courses/cs11/material/cpp/donnie/cpp-ops.html
+		// define  the equality statement
+		Matrix<T>& operator=(const Matrix<T> &Other);
+
+		// Next we declare the compound equality statements
+		Matrix<T>& operator+=(const Matrix<T> &Other);
+		Matrix<T>& operator-=(const Matrix<T> &Other);
+		Matrix<T>& operator*=(const Matrix<T> &Other);
+
+		// Declare the normal operations
+		Matrix<T>& operator+(const Matrix<T> &Other);
+		Matrix<T>& operator-(const Matrix<T> &Other);
+		Matrix<T>& operator*(const Matrix<T> &Other);
+
+		// Now declare the comparison operations
+		bool operator==(const Matrix<T> &Other);
+		bool operator!=(const Matrix<T> &Other);
+
+
+		// Next we declare the usual Matrix Functions
+		Matrix<T>& RowEchelon(); // Return another Matrix which is a RowEchelon form of this
+		Matrix<T>& Inverse();
+		int Rank();
+		T Determinant();
+		
+		vector<T> GetRow(int row);
+		vector<T> GetCol(int col);
+
+	private:
+		int nRows;
+		int mCols;	
+		
+};
+
+#endif
+
+
+
+
+
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -15,16 +15,126 @@
  */
 
 #include "matrix.h"
-
 /*------------------------------------------------------
- * ExampleFunction -- Just a foobar function
- * Args:  None
+ * Matrix -- init function of the Matrix Class
+ * Args: int nRows, int mCols
  * Returns: void
- * Throws:
- * See:
- * Bugs:
  *------------------------------------------------------*/
-void ExampleFunction( )
+template <typename T> 
+Matrix<T>::void Matrix(int n, int m)
 {
-	//some blah
+	nRows = n; mCols = m;
+	matrix = new T*[nRows];
+
+	for (int i=0;i<nRows; i++)
+		matrix[i] = new T[nCols];
 }
+
+
+template <typename T>
+Matrix<T>::Matrix<T>& operator=(const Matrix<T> &Other)
+{
+	
+
+}
+
+
+template <typename T> 
+Matrix<T>::Matrix<T>& operator+=(const Matrix<T> &Other)
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::Matrix<T>& operator-=(const Matrix<T> &Other)
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::Matrix<T>& operator*=(const Matrix<T> &Other)
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::Matrix<T>& operator+(const Matrix<T> &Other)
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::Matrix<T>& operator-(const Matrix<T> &Other)
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::Matrix<T>& operator*(const Matrix<T> &Other)
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::bool operator==(const Matrix<T> &Other)
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::bool operator!=(const Matrix<T> &Other)
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::Matrix<T>& RowEchelon()
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::Matrix<T>& Inverse()
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::int Rank()
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::T Determinant()
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::vector<T> GetRow(int row)
+{
+
+
+}
+
+template <typename T> 
+Matrix<T>::vector<T> GetCol(int col)
+{
+
+
+}
+
+
